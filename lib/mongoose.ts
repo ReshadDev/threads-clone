11122fa90817b813c5865d5cbf356172372f9d90
ReshadDev <reshadqarayev44@gmail.com>
2023-10-11 20:24:49 +0400

    update user profile

---
+++ lib/mongoose.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (!process.env.MONGODB_URL) {
+    console.log("MONGODB_URL not found in .env file");
+  }
+
+  if(isConnected){
+    console.log("Already connected to DB");
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URL!)
+    isConnected = true;
+    console.log("Connected to DB"); 
+    
+  } catch (error) {
+    console.log("Error connecting to DB", error)
+  }
+};
